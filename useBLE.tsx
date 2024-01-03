dec5e2d86a2167ed5328318ccea83af6cc03dafb
Gustavo <luis.oliveira@technolog.com.br>
2024-01-03 09:10:17 -0300

    Refatoração, limpeza de variáveis

--- useBLE.tsx
+++ useBLE.tsx
@@ -12,55 +12,9 @@ import base64 from "react-native-base64";
 
 import * as ExpoDevice from "expo-device";
 
-const GALILEOSKY_UUID = "0000a441-0000-1000-8000-00805f9b34fb" //SERVICE UUID
-const GALILEOSKY_READ_UUID = "0783b03e-8535-b5a0-7140-a304d2495cb7"; //SPS
-const GALILEOSKY_NOTIFY_UUID = "0783b03e-8535-b5a0-7140-a304d2495cb8"; //SERVER_TX, 250B, Cliente deve assinar SERVER_TX para receber mensagens
-const GALILEOSKY_WRITENORESPONSE_UUID = "0783b03e-8535-b5a0-7140-a304d2495cba"; //SERVER_RX, 250B, Cliente deve assinar SERVER_RX para enviar mensagens
-const GALILEOSKY_WRITENORESPONSE_NOTIFY_UUID = "0783b03e-8535-b5a0-7140-a304d2495cb9"; //FLOW_CTRL, 1B, não é usado
-
-/****** Formato da mensagem / 250 bytes *******
-| Byte nº | Length | Value |    Descrição     |
------------------------------------------------
-|   1     |    1   | 0x41  |                  |
-|   2     |    1   | 0xA4  | Cabeçalho da     |
-|   3     |    1   | 0x12  | Mensagem         |
-|   4     |    1   | 0x21  |                  |
-|  ...    |   ...  |       | Pacote principal |
-|   n     |   15   | 0x03  |    IMEI          |
-|  ...    |   ...  |       | Pacote principal |
------------------------------------------------
-
-Cabeçalho retornando: QaQSIQ== ()
-https://cryptii.com/pipes/base64-to-binary
-
-QaQSIQ==
-AR4AAzg2MjM=
-MTEwNjEzMTU=
-MjQ2EAAAQEI=
-K0GYXkKADkMmIMA=
-
-Pacote: 
-Header BLE:         41 A4 12 21 
-Header PCT:         01 
-Length:             1e 00 
-IMEI:               03 38 36 32 33 31 31 30 36 31 33 31 35 32 34 36 
-Number of record:   10 00 00 
-Status device:      40 42 2B 
-Supply Voltage:     41 98 5e 
-Battery Voltage:    42 80 0e 
-Inside temperature: 43 26
-CRC:                20 c0
-
-418d5e42780ec25f41a41221011c000338363233
-1b419b5e42780e8b9c
-
-41a41221
-011e00
-03 38 36 32 33 31 31 30 36 31 
-33 31 35 32 34 36 10 00 00 40 
-40 2b 41 a8 5e 42 79 0e 43 27
-4055
-*/
+const GALILEOSKY_READ_UUID = "0783b03e-8535-b5a0-7140-a304d2495cb7";
+const GALILEOSKY_NOTIFY_UUID = "0783b03e-8535-b5a0-7140-a304d2495cb8";
+const NAME_GALILEO = "GS7X 86"
 
 interface BluetoothLowEnergyApi {
     requestPermissions(): Promise<boolean>;
@@ -73,8 +27,9 @@ interface BluetoothLowEnergyApi {
     size: number;
 }
 
-const magic_header = 1101271585;
-const magicHeaderLen = 4;
+const magic_header = 1101271585; // 0x41 0xA4 0x12 0x21
+const magic_header_len = 4;
+const android_api_31 = 31; // Versão 12
 
 function useBLE(): BluetoothLowEnergyApi {
 
@@ -119,7 +74,7 @@ function useBLE(): BluetoothLowEnergyApi {
 
     const requestPermissions = async () => {
         if (Platform.OS === "android") {
-            if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+            if ((ExpoDevice.platformApiLevel ?? -1) < android_api_31) {
                 const granted = await PermissionsAndroid.request(
                     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
                     {
@@ -143,14 +98,17 @@ function useBLE(): BluetoothLowEnergyApi {
     const isDuplicateDevice = (devices: Device[], nextDevice: Device) =>
         devices.findIndex((device) => nextDevice.id === device.id) > -1;
 
+    const searchDeviceRSSI = async (device: Device) => {
+        return await device.readRSSI();
+    }
+
     const scanForPeripherals = () =>
         bleManager.startDeviceScan(null, null, (error, device) => {
             if (error) {
                 console.log(error);
             }
-            if (device && device.id?.includes("80:EA:CA:00:1E:43")) {
-                // if (device) {
-                console.log(device)
+            if (device && device.name?.includes(NAME_GALILEO)) {
+                console.log(device.rssi);
                 setAllDevices((prevState: Device[]) => {
                     if (!isDuplicateDevice(prevState, device)) {
                         return [...prevState, device];
@@ -213,7 +171,7 @@ function useBLE(): BluetoothLowEnergyApi {
 
         const rawData = decodeBase64(characteristic?.value ?? '');
 
-        if (rawData.length >= magicHeaderLen) {
+        if (rawData.length >= magic_header_len) {
             if (rawData.readUInt32BE(0) === magic_header) {
                 setGalileoDataBuffer(Buffer.alloc(0));
                 setSize(0);
@@ -223,76 +181,6 @@ function useBLE(): BluetoothLowEnergyApi {
         setGalileoDataBuffer((prevState) => Buffer.concat([prevState, rawData]));
         setSize((prevState) => prevState + rawData.length)
     };
-
-    // const onGalileoUpdate = (
-    //     error: BleError | null,
-    //     characteristic: Characteristic | null
-    // ) => {
-    //     if (error) {
-    //         console.log(error);
-    //         return -1;
-    //     } else if (!characteristic?.value) {
-    //         console.log("No Data was received");
-    //         return -1;
-    //     }
-
-    //     const rawData = decodeBase64(characteristic?.value ?? '');
-
-    //     if (onMagicHeader) {
-    //         if (!header) {
-    //             const headerBuf = rawData.slice(0, mainHeaderLen);
-    //             const tag = headerBuf[0];
-    //             setActualSize(rawData.length - mainHeaderLen)
-
-    //             if (tag !== 0x01) {
-    //                 console.log("Pacote não está no formato Galileo");
-    //                 return;
-    //             }
-
-    //             const pkgLen = headerBuf.readUInt16LE(1);
-    //             setGalileoData((prevState) => [...prevState, rawData]);
-    //             setSize(pkgLen)
-    //             setHeader(true)
-    //         } else {
-
-    //             setActualSize(actualSize + rawData.length)
-    //             setGalileoData((prevState) => [...prevState, rawData])
-
-    //             if (actualSize === size + crcLen) {
-
-    //                 // decodificar aqui
-    //                 setGalileoDataBuffer(galileoData)
-    //                 setActualSize(0)
-    //                 setSize(0)
-    //                 setHeader(false)
-    //                 setGalileoData([])
-    //                 setOnMagicHeader(false)
-    //             }
-    //             return;
-    //         }
-
-    //     } else {
-
-    //         if (rawData.length < magicHeaderLen) {
-    //             console.log("Tamanho do magic header incompatível")
-    //             return;
-    //         }
-
-    //         if (!ignoreFirstPkg) {
-    //             console.log("Primeiro pacote não contado")
-    //             setIgnoreFirstPkg(true);
-    //             return;
-    //         }
-
-    //         const magicHeaderBuffer = rawData.slice(0, magicHeaderLen);
-
-    //         if (magicHeaderBuffer.readUInt32BE() === magic_header) {
-    //             setOnMagicHeader(true);
-    //             setGalileoData((prevState) => [...prevState, magicHeaderBuffer]);
-    //             return;
-    //         }
-    //     }
-    // };
 
     const startStreamingData = async (device: Device) => {
         if (device) {
